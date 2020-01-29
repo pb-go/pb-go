5fe3161855d13449548382d1cfa3ea966dfb8710
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2020-01-29 10:40:39 +0800

    stash: save working progress

--- databaseop/mongoop.go
+++ databaseop/mongoop.go
@@ -1,6 +1,45 @@
 package databaseop
 
 import (
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
 )
+
+type MongoDB struct {
+	DbCli mongo.Client
+	DbURI string
+	DefaultDB string
+	DefaultColl string
+	DefaultTimeout time.Time
+	BsonData chan bson.M
+}
+
+func (mdbc MongoDB) connect2DB(dbURI string) error{
+
+}
+
+func (mdbc MongoDB) checkConn(dbCli interface{}) error{
+
+}
+
+func (mdbc MongoDB) testCollectionNIndex(dbCli interface{}, dbName string, collName string) int{
+
+}
+
+func (mdbc MongoDB) itemCreate(dbCli interface{}, dbName string, collName string, data interface{}) int{
+
+}
+
+func (mdbc MongoDB) itemUpdate(dbCli interface{}, dbName string, collName string, data interface{}) int{
+
+}
+
+func (mdbc MongoDB) itemDelete(dbCli interface{}, dbName string, collName string, data interface{}) int{
+
+}
+
+func (mdbc MongoDB) itemRead(dbCli interface{}, dbName string, collName string, data interface{}) int{
+
+}
