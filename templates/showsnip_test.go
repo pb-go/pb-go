dd299a77315bd795422d4b3c1e044e3443fba040
STRRL <str_ruiling@outlook.com>
2020-01-29 12:20:59 +0800

    :construction: using more complexd test case for showsnip

--- templates/showsnip_test.go
+++ templates/showsnip_test.go
@@ -2,6 +2,7 @@ package templates
 
 import (
 	"bytes"
+	"fmt"
 	"html/template"
 	"io/ioutil"
 	"testing"
@@ -11,11 +12,15 @@ func TestRendingSnippets(t *testing.T) {
 	template, err := template.New("test").ParseFiles("showsnip.html.tmpl")
 	if err == nil {
 		var buffer bytes.Buffer
-		err := template.ExecuteTemplate(&buffer, "content", "foobar")
+		err := template.ExecuteTemplate(&buffer, "content", "%bool;\n\n &foo #bar*test <xml>")
 		if err == nil {
 			file, err := ioutil.ReadFile("test_expect_showsnip.html")
 			if err == nil {
 				if buffer.String() != string(file) {
+					fmt.Println("expect is:")
+					fmt.Println(string(file))
+					fmt.Println("but actual is:")
+					fmt.Println(buffer.String())
 					t.Fail()
 				}
 			}
