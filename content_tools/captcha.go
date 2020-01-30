c09360a5b4ce5a650f1af16cbff00233ebb6963d
STRRL <str_ruiling@outlook.com>
2020-01-30 22:35:20 +0800

    :sparkles: recaptcha server side invalidation

--- content_tools/captcha.go
+++ content_tools/captcha.go
@@ -1,7 +1,34 @@
 package content_tools
 
 import (
-	"github.com/imroc/req"
-	"github.com/kmahyyg/pb-go/main"
+	"encoding/json"
+	"github.com/kmahyyg/pb-go/config"
+	"net/http"
+	"net/url"
+	"time"
 )
 
+const recaptchaUrl = "https://www.google.com/recaptcha/api/siteverify"
+
+type ReCaptchaResponse struct {
+	Success     bool      `json:"success"`
+	ChallengeTS time.Time `json:"challenge_ts"`
+	Hostname    string    `json:"hostname"`
+	ErrorCodes  []string  `json:"error-codes"`
+}
+
+func VerifyRecaptcha(config config.Recaptcha, recaptchaResponse string, remoteIp string) (result bool, err error) {
+	response, err := http.PostForm(recaptchaUrl, url.Values{
+		"secret":   {config.Secret_key},
+		"response": {recaptchaResponse},
+		"remoteip": {remoteIp},
+	})
+	if err == nil {
+		defer response.Body.Close()
+		reCaptchaResponse := ReCaptchaResponse{}
+		err = json.NewDecoder(response.Body).Decode(reCaptchaResponse)
+		result = reCaptchaResponse.Success
+		return
+	}
+	return
+}
