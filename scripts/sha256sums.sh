e24e264288e46093d84195b2ba9e4363a737cd20
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2020-02-05 17:58:20 +0800

    style(a lot): reformat, fix "Error Prone" in Codacy
    Signed-off-by: kmahyyg <16604643+kmahyyg@users.noreply.github.com>

--- scripts/sha256sums.sh
+++ scripts/sha256sums.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-SHA256SUMS=""
+SHA256SUMS="Binary Assets Checksum: \n"
 cd output || exit
 for FILE in *
 do
-    SHA256SUMS="${SHA256SUMS} \n $(sha256sum ${FILE})"
+    SHA256SUMS+="$(sha256sum ${FILE})"
+    SHA256SUMS+="\n"
 done
 cd ..
 echo -e "${SHA256SUMS}" | tee output/sha256sums.txt
