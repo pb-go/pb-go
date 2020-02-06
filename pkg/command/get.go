222fc56ead59319e0a34fec0318d43e667e74f97
STRRL <str_ruiling@outlook.com>
2020-02-07 00:45:13 +0800

    feat(pb-cli): get command

--- pkg/command/get.go
+++ pkg/command/get.go
@@ -1,10 +1,43 @@
 package command
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/valyala/fasthttp"
+	"os"
+)
 
-func GetCommand() *cobra.Command {
-	return &cobra.Command{
+var (
+	passwordForGet string
+	getCmd         = &cobra.Command{
 		Use:   "get",
 		Short: "Fetching data from patesbin with id.",
+		Args:  cobra.MinimumNArgs(1),
+		RunE:  get,
 	}
+)
+
+func GetCommand() *cobra.Command {
+	getCmd.Flags().StringVarP(&passwordForGet, "password", "p", "", "Optional. Provide password for private share.")
+	return getCmd
+}
+
+func get(command *cobra.Command, args []string) (err error) {
+
+	url := viper.Get("host").(string) + "/" + args[0]
+
+	if passwordForGet != "" {
+		url += "?p=" + passwordForGet
+	}
+
+	code, body, err := fasthttp.Get(make([]byte, 0), url)
+
+	fmt.Fprintf(os.Stderr, "Server Response:\n")
+	fmt.Fprintf(os.Stderr, "Http Status Code: %d\n", code)
+	fmt.Fprintf(os.Stderr, "Http Response Body:\n")
+	fmt.Printf(string(body))
+	fmt.Fprintf(os.Stderr, "\n")
+
+	return
 }
